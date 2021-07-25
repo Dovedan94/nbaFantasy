e87bcfc6fd5a1d462d11b01ff7dc730cc42f358c
dovedan <dovedanmilosh@gmail.com>
2021-07-25 23:52:57 +0200

    Implemented points and players scraping functions

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from bs4 import BeautifulSoup
 from enums import LINK
 
 page = requests.get(LINK)
-print(page.status_code)
-
 soup = BeautifulSoup(page.content, "html.parser")
 
 totals_table = soup.find("table", id="totals")
+
 players_column = totals_table.find_all("td", class_="left")
+points_column = totals_table.find_all("tr")
+
+points = []
+for point in points_column[1:-1]:
+    points.append(point.find_all("td")[-1].get_text())
 
 players = []
-for i in players_column[:-1]:
-    players.append(i.get_text())
+for player in players_column[:-1]:
+    players.append(player.get_text())
